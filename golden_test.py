6fd92ba957ef9a87bc08fdeb270804dc349974ba
arslanefimov <dns230717rus@gmail.com>
2024-05-28 17:39:49 +0300

    run test hello_username and hello_world

--- golden_test.py
+++ golden_test.py
@@ -8,41 +8,31 @@ import machine
 import pytest
 import translator
 
-@pytest.fixture
-def temp_dir():
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        yield tmpdirname
 
 @pytest.mark.golden_test("golden/*.yml")
-def test_translator_asm_and_machine(golden, caplog, temp_dir):
+def test_translator_asm_and_machine(golden, caplog):
     """Почти полная копия test_translator_and_machine из golden_bf_test. Детали
     см. там."""
     caplog.set_level(logging.DEBUG)
 
-    source = os.path.join(temp_dir, "source.asm")
-    input_stream = os.path.join(temp_dir, "input.txt")
-    target = os.path.join(temp_dir, "target.o")
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        source = os.path.join(tmpdirname, "source.asm")
+        input_stream = os.path.join(tmpdirname, "input.txt")
+        target = os.path.join(tmpdirname, "target.o")
 
-    # Создаем исходные файлы
-    with open(source, "w", encoding="utf-8") as file:
-        file.write(golden["in_source"])
-    with open(input_stream, "w", encoding="utf-8") as file:
-        file.write(golden["in_stdin"])
+        with open(source, "w", encoding="utf-8") as file:
+            file.write(golden["in_source"])
+        with open(input_stream, "w", encoding="utf-8") as file:
+            file.write(golden["in_stdin"])
 
-    # Перенаправляем stdout
-    stdout = io.StringIO()
-    with contextlib.redirect_stdout(stdout):
-        # Выполняем translator
-        translator.main(source, target)
-        print("============================================================")
-        # Выполняем machine
-        machine.main(target, input_stream)
+        with contextlib.redirect_stdout(io.StringIO()) as stdout:
+            translator.main(source, target)
+            print("============================================================")
+            machine.main(target, input_stream)
 
-    # Читаем выходной файл
-    with open(target, encoding="utf-8") as file:
-        code = file.read()
+        with open(target, encoding="utf-8") as file:
+            code = file.read()
 
-    # Проверяем результаты
-    assert code == golden.out["out_code"]
-    assert stdout.getvalue() == golden.out["out_stdout"]
-    assert caplog.text == golden.out["out_log"]
+        assert code == golden.out["out_code"]
+        assert stdout.getvalue() == golden.out["out_stdout"]
+        assert caplog.text == golden.out["out_log"]
